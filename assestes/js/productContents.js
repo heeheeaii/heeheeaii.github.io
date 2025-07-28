6583475fd40357c274b63a92525beb021eff9725
hee <heeheeaiii@163.com>
2025-07-28 23:15:29 +0800

    1.2

---
+++ assestes/js/productContents.js
@@ -0,0 +1,67 @@
+// 产品内容列表
+const productContents = [
+    ` Beself 1.1
+
+1 新增网站复制，允许网页管理，更新获取
+
+
+
+桌面下载地址: https://wwxb.lanzoul.com/iAdD231yaqkf
+
+安卓下载地址: https://wwxb.lanzoul.com/iFwAh31yajsb
+`
+    ,
+    `🚀 Beself 1.0 正式发布
+
+非常激动地向大家宣布，Beself产品已重磅上线！在设计和功能层面，带来了如下重要特性：
+
+
+
+🎬 视频彻底禁用
+
+永远禁止一切视频内容，无论任何情况，都不会让视频加载或播放，为您打造更高效纯粹的体验。
+
+
+
+✅ 显式控制访问
+
+只有明确允许的网站才可访问。除非经过您的允许，否则总是无法打开网站。
+
+
+
+🗑️ 删除优先策略
+
+清理操作至上。如果您多次选择删除同一个网站，那么直接让它永远消失吧。
+
+
+
+🌙 强制黑暗模式
+
+黑暗模式已支持，不论白天还是夜晚，都能拥有舒适护眼的视觉体验。
+
+💻📱 跨平台发布
+
+桌面版与移动版同步上线，提供无缝一致的多终端体验，让您的工作与生活更加便捷流畅。
+
+---
+
+感谢每一位用户的关注和支持！
+现在就来体验吧！
+
+桌面下载地址: https://wwxb.lanzoul.com/iTiAW31r3sfi
+
+安卓下载地址: https://wwxb.lanzoul.com/iK4xc31r3jgf
+`,
+];
+// 轮播图图片数组
+const carouselImages = [
+    {
+        src: 'assestes/img/home-comp.jpg'
+    },
+    {
+        src: 'assestes/img/func-comp.jpg'
+    },
+    {
+        src: 'assestes/img/search-comp.jpg'
+    }
+];
